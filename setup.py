dfbddd5a1cd2280b43e4b617b6ba974fc38f582b
Johannes Hansen <johannes.hansen@ed.ac.uk>
2018-08-06 13:34:34 +0200

    use setup.cfg, deploy with travis

--- setup.py
+++ setup.py
@@ -5,11 +5,6 @@ import os
 import shutil
 
 
-def readme():
-    with open('README.md') as f:
-        return f.read()
-
-
 def init_config():
     src = 'esahub/config.yaml'
     dst = os.path.expanduser('~/.esahub.conf')
@@ -28,33 +23,5 @@ class BuildPyCommand(setuptools.command.build_py.build_py):
 setuptools.setup(
     cmdclass={
         'build_py': BuildPyCommand,
-    },
-    name='esahub',
-    version='0.1',
-    description='A Python module for downloading ESA satellite data',
-    long_description=readme(),
-    keywords='esahub sentinel satellite',
-    url='http://github.com/jnhansen/esahub',
-    author='Johannes Hansen',
-    author_email='johannes.niklas.hansen@gmail.com',
-    license='MIT',
-    packages=setuptools.find_packages(),
-    install_requires=[
-        'pyyaml',
-        'numpy',
-        'lxml',
-        'pyproj',
-        'shapely',
-        'netCDF4',
-        'python-dateutil',
-        'pytz',
-        'tqdm'
-    ],
-    setup_requires=['pytest-runner'],
-    entry_points={
-        'console_scripts': ['esahub=esahub.cli:main'],
-    },
-    include_package_data=True,
-    zip_safe=False,
-    tests_require=['pytest']
+    }
 )
